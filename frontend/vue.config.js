5232a8a4276298f87b6889ae37192a5ad35cf08a
Joey Ren <tsuyiren@gmail.com>
2020-06-22 18:11:35 +0800

    [開發] vue 框架加入

---
+++ frontend/vue.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    runtimeCompiler: true,
+    // 在專案開發中如果呼叫 API 時會 pass 給這個 proxy 網址
+    // 這邊就用前面以 Valet 建立的網站網址
+    devServer: {
+        public: '0.0.0.0:4000',
+        port: 4000,
+        disableHostCheck: true,
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        proxy: {
+            '/api': {
+                target: 'http://192.168.64.4',
+                secure: false,
+                changeOrigin: true
+            }
+        }
+    },
+
+    // 建置前端靜態檔案時要擺放的目錄
+    // 在 package.json 也要調整 "build" 這個 script
+    outputDir: '../public',
+
+    // 開發階段修改 index.html 來讓 js/css 可以作用
+    // 上線階段則會修改 Laravel 的樣版
+    indexPath: process.env.NODE_ENV === 'production'
+        ? '../resources/views/index.blade.php'
+        : 'index.html'
+}
+
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('layout', function ()
+{
+    return response(['success' => 'Y'], Response::HTTP_OK);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
